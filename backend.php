94496436ca0a9dd902eed3bb201a2165c4222840
makoto_kozono <oceandepthz@gmail.com>
2019-04-15 17:16:40 +0900

    modify

--- backend.php
+++ backend.php
@@ -66,6 +66,7 @@
 	$update_intervals = array(
 		0   => __("Default interval"),
 		-1  => __("Disable updates"),
+		5   => __("5 minutes"),
 		15  => __("15 minutes"),
 		30  => __("30 minutes"),
 		60  => __("Hourly"),
@@ -76,6 +77,7 @@
 
 	$update_intervals_nodefault = array(
 		-1  => __("Disable updates"),
+		5   => __("5 minutes"),
 		15  => __("15 minutes"),
 		30  => __("30 minutes"),
 		60  => __("Hourly"),
